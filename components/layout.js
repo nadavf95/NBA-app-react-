828801e0c8dc61ba379cc99334f6d3ec54c8cde4
Nadav Fuchs <nadavf95@gmail.com>
2022-12-05 15:47:03 +0200

    new files

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Footer from "./footer";
 import Navbar from "./navbar";
+import sort_sec from "./sort_sec";
 
 // This is our base page layout - See it is used in pages_app.js
 export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
+      <sort_sec />
       <main className="container">
         {children}
       </main>
