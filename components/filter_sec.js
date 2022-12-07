2cfeba35e523ae693d1d4895bd07a0ba6eaac1fd
Nadav Fuchs <nadavf95@gmail.com>
2022-12-07 11:57:22 +0200

    sort_sec

--- components/filter_sec.js
+++ components/filter_sec.js
@@ -1,13 +1,13 @@
 
-export default function Layout({ children }) {
+export default function filter_sec({ children }) {
     return (
         <div class="content">
             <h3>coast</h3>
             <select id="data-filter">
                 <option value="none">none</option>
                 <option value="East">East</option>
-                <option value="West">West</option>z
+                <option value="West">West</option>
             </select>
         </div>  
-    )
+    );
 }
