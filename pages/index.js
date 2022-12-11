9faf0a63cc4429f42b0b9d9a158ba5ef408ef7d6
Nadav Fuchs <nadavf95@gmail.com>
2022-12-11 16:29:47 +0200

    changes

--- pages/index.js
+++ pages/index.js
@@ -9,18 +9,18 @@ import { ChangeHandler } from "../utilities";
 // And showing some loading screen(s) while waiting for the data to be ready
 export default function IndexPage() {
   
-  const date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let currentDate = `${year}-${month}-${day}`;
-  console.log(currentDate);
+  // const date = new Date();
+  // let day = date.getDate();
+  // let month = date.getMonth() + 1;
+  // let year = date.getFullYear();
+  // let currentDate = `${year}-${month}-${day}`;
+  // console.log(currentDate);
 
 
   const [games, setGames] = useState({});
   const [sort_key, setSortKey] = useState("a-z");
   const [filter_key, setFilterKey] = useState("none");
-  setGames(ChangeHandler(setGames, "date", currentDate));
+  // setGames(ChangeHandler(setGames, "date", currentDate));
   return (
     <>
       <div className="row">
