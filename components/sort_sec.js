03c605f6f924ad676b482103b3eef880501183a4
Nadav Fuchs <nadavf95@gmail.com>
2022-12-14 17:03:03 +0200

    fix navbar

--- components/sort_sec.js
+++ components/sort_sec.js
@@ -2,7 +2,7 @@
 export default function SortSec({ChangeHandler}) {
     return (
         <div className="contentSort">
-            <h3>sort by</h3>
+            <h2>sort by:</h2>
             <select onChange={(e) => ChangeHandler("sort",e.target.value)}>
                 <option value="a-z">a-z</option>
                 <option value="top score">top score</option>
